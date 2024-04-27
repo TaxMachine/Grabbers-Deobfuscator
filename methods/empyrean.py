e724d292c89b4890b8a59af43818adfece1c672f
TaxMachine <tax.machine348@gmail.com>
2024-04-26 23:58:26 -0400

    security update added input before empyrean obfuscation bypass to not accidentally run malicious code

--- methods/empyrean.py
+++ methods/empyrean.py
@@ -7,6 +7,9 @@ class VespyDeobf:
 
     def Deobfuscate(self):
         config_file = os.path.join(self.extractiondir, "PYZ-00.pyz_extracted", "config.pyc")
+
+        if input("This method will execute the code found in the config.pyc to bypass the obfuscation. Make sure to run this in a virtual machine or a container in case there is malicious code, do you want to run the code (y/n)?: ").lower() != "y":
+            return ""
         
         spec = importlib.util.spec_from_file_location("config", config_file)
         module = importlib.util.module_from_spec(spec)
